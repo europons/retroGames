7d22bfa1ddf6d1d96fa8f9a9b43ef43eaebc6150
Javi <europons@gmail.com>
2026-01-28 18:48:39 +0100

    Eliminado el div game-container

--- src/juegos/super-hopper/pantallas/pantalla-inicio.jsx
+++ src/juegos/super-hopper/pantallas/pantalla-inicio.jsx
@@ -1,13 +1,12 @@
 export default function PantallaInicio(props) {
     return (
-        <div id="game-screen">
-            <div id="score-display" class="hidden">SCORE: <span id="score">{props.score}</span></div>
-            <div id="start-screen" class="center-screen">
-                <h1 class="title">SUPER HOPPER</h1>
-                <p class="subtitle">MY CUTE ADVENTURE</p>
+        <div>
+            <div id="score-display" className="hidden">SCORE: <span id="score">{props.score}</span></div>
+            <div id="start-screen" className="center-screen">
+                <h1 className="title">SUPER HOPPER</h1>
+                <p className="subtitle">MY CUTE ADVENTURE</p>
                 <button id="start-btn" onClick={props.iniciarPartida}>PRESS START</button>
             </div>
-            <div id="game-container"></div>
         </div>
     );
 }
