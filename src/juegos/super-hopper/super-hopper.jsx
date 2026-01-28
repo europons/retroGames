a9e32b47649ad0cd05cc79668b5ffe25a0132d71
Javi <europons@gmail.com>
2026-01-28 09:51:42 +0100

    Creado componente vacio

---
+++ src/juegos/super-hopper/super-hopper.jsx
@@ -0,0 +1,8 @@
+export default function SuperHopper() {
+    return (
+        <div>
+            <h2>Super Hopper</h2>
+            <p>¡Bienvenido a Super Hopper! (Juego en desarrollo)</p>
+        </div>
+    );
+}
